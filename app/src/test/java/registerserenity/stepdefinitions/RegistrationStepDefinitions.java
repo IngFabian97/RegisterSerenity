233e83b78ada1ef05a5e6e894c386cee9c574179
IngFabian97 <fguarin@bancolombia.com.co>
2025-05-23 16:15:42 -0500

    Config project

---
+++ app/src/test/java/registerserenity/stepdefinitions/RegistrationStepDefinitions.java
@@ -0,0 +1,13 @@
+package registerserenity.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import net.serenitybdd.screenplay.Actor;
+
+public class RegistrationStepDefinitions {
+
+    @Given("{actor} wants to register sign up in the aplication")
+    public void wanstToSignUpInTheApplication(Actor actor) {
+   
+    }
+
+}
---
+++ app/src/test/resources/features/registration.feature
@@ -0,0 +1,4 @@
+Feature: Registration
+
+  Scenario: Succesful Registration
+    Given Fabian wants to register sign up in the aplication
