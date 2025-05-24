9f3f636fa79d1da31b9aaa72fc6ef179f72e08b3
IngFabian97 <fguarin@bancolombia.com.co>
2025-05-23 20:08:27 -0500

    Init scenario one

--- app/src/test/java/registerserenity/stepdefinitions/RegistrationStepDefinitions.java
+++ app/src/test/java/registerserenity/stepdefinitions/RegistrationStepDefinitions.java
@@ -1,13 +1,56 @@
 package registerserenity.stepdefinitions;
 
+import java.net.URISyntaxException;
+
 import io.cucumber.java.en.Given;
 import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.actions.*;
+import net.serenitybdd.screenplay.ensure.Ensure;
+import net.serenitybdd.screenplay.targets.Target;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class RegistrationStepDefinitions {
 
     @Given("{actor} wants to register sign up in the aplication")
-    public void wanstToSignUpInTheApplication(Actor actor) {
-   
+    public void wanstToSignUpInTheApplication(Actor actor) throws URISyntaxException {
+
+        String name = "Fabian";
+        String lastname = "Guarin";
+        String age = "28";
+        String country = "Colombia";
+        String email = "fabian@email.com";
+        String dayWork = "Lunes";
+
+        actor.attemptsTo(
+                Open.url("http://127.0.0.1:5501/register.html"),
+                WaitUntil.the("//input[@id='name']", isVisible()).forNoMoreThan(10).seconds(),
+                Enter.theValue(name).into("//input[@id='name']"),
+                WaitUntil.the("//input[@id='last-name']", isVisible()).forNoMoreThan(10).seconds(),
+                Enter.theValue(lastname).into("//input[@id='last-name']"),
+                WaitUntil.the("//input[@id='age']", isVisible()).forNoMoreThan(10).seconds(),
+                Enter.theValue(age).into("//input[@id='age']"),
+                WaitUntil.the("//select[@id='country']", isVisible()).forNoMoreThan(10).seconds(),
+                SelectFromOptions.byVisibleText(country).from("//select[@id='country']"),
+                WaitUntil.the("//input[@id='sex-m']", isVisible()).forNoMoreThan(10).seconds(),
+                Click.on("//input[@id='sex-m']"),
+                WaitUntil.the("//input[@id='email']", isVisible()).forNoMoreThan(10).seconds(),
+                Enter.theValue(email).into("//input[@id='email']"),
+                WaitUntil.the("//input[@id='monday']", isVisible()).forNoMoreThan(10).seconds(),
+                Click.on("//input[@id='monday']"),
+                Upload.theClasspathResource("pictures/OIP.jpg")
+                        .to(Target.the("picture field").locatedBy("//input[@id='picture']")),
+                WaitUntil.the("//button[@id='save-btn']", isVisible()).forNoMoreThan(10).seconds(),
+                Click.on("//button[@id='save-btn']"),
+                Switch.toWindowTitled("Summary"),
+                WaitUntil.the("//strong[contains(text(), 'Nombre')]/parent::p", isVisible()).forNoMoreThan(10)
+                        .seconds(),
+                Ensure.that(Target.the("Name element")
+                        .locatedBy("//strong[contains(text(), 'Nombre')]/parent::p")).text().containsIgnoringCase(name),
+                Ensure.that(Target.the("Last name element")
+                        .locatedBy("//strong[contains(text(), 'Apellido')]/parent::p")).text()
+                        .containsIgnoringCase(lastname));
+
     }
 
 }
