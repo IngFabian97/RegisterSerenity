102296971c9dad22f1cc8ff40cf896936a84a051
IngFabian97 <fguarin@bancolombia.com.co>
2025-05-27 00:43:27 -0500

    Environments URLs

--- app/src/test/java/registerserenity/stepdefinitions/RegistrationStepDefinitions.java
+++ app/src/test/java/registerserenity/stepdefinitions/RegistrationStepDefinitions.java
@@ -5,18 +5,13 @@ import java.net.URISyntaxException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.java.en_old.Ac;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.actions.*;
-import net.serenitybdd.screenplay.ensure.Ensure;
-import net.serenitybdd.screenplay.targets.Target;
-import net.serenitybdd.screenplay.waits.WaitUntil;
 import registerserenity.tasks.registration.CheckNewAccountCreated;
 import registerserenity.tasks.registration.DoNotSendAllRequiredInformation;
 import registerserenity.tasks.registration.RegisterUser;
 import registerserenity.tasks.registration.ShouldSeeAllFieldsAreRequired;
-
-import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
+import registerserenity.ui.registration.RegisterPage;
 
 public class RegistrationStepDefinitions {
 
@@ -31,11 +26,11 @@ public class RegistrationStepDefinitions {
         public void wanstToSignUpInTheApplication(Actor actor) {
 
                 actor.attemptsTo(
-                                Open.url("http://127.0.0.1:5501/register.html"));
+                                Open.browserOn(new RegisterPage()));
         }
 
         @When("{actor} send the required information to sign up")
-        public void sendsRequiredInformationToSignUp(Actor actor) throws URISyntaxException {
+        public void sendsRequiredInformationToSignUp(Actor actor) {
 
                 actor.attemptsTo(
                                 new RegisterUser(name, lastname, age, country, email, dayWork));
---
+++ app/src/test/java/registerserenity/ui/registration/RegisterPage.java
@@ -0,0 +1,9 @@
+package registerserenity.ui.registration;
+
+import net.serenitybdd.annotations.DefaultUrl;
+import net.serenitybdd.core.pages.PageObject;
+
+@DefaultUrl("http://127.0.0.1:5501")
+public class RegisterPage extends PageObject {
+
+}
